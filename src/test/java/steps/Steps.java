d73f1ca9abede727e19eb6e83ce4c89387f3b8d0
Anthony Rozon <arozonm@gmail.com>
2017-11-09 15:01:54 -0400

    Added step definition methods

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -1,4 +1,38 @@
 package steps;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
 public class Steps {
+
+    @Given("^Step one$")
+    public void step_one() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Step two$")
+    public void step_two() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Step three$")
+    public void step_three() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Step four$")
+    public void step_four() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Step five$")
+    public void step_five() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
