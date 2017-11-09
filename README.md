0d650e3e3680b50bbb49146ec189f97284406540
Anthony Rozon <arozonm@gmail.com>
2017-11-09 12:02:37 -0400

    Added Runner class

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # selenium-cucumber-maven
 A boilerplate to write automated testing scenarios with Cucumber and Selenium using Java with Maven
+
+Needed plugins when using IntelliJ
+
+    Cucumber for Java
+    Gherkin
+    TestNG
---
+++ src/test/java/Runner.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        format = {"pretty", "html:target/Reports"}
+)
+public class Runner {
+}
